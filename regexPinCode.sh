cf2c3089f6c0b05f6c54b5fcd922a313c88f2edc
rajkush211 <rajkush211.rk@gmail.com>
2020-03-27 06:26:26 +0530

    ADDED: Regex pattern to validate 6 Digit Code

---
+++ regexPinCode.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+#Description: Regex pattern to validate PIN Code.
+#Author: Raj Kush
+#Date: 27 March 2020
+clear
+
+shopt -s extglob
+
+#VARIABLES
+pinCode=0
+
+function validatePinCode() {
+	local pin=$1
+	local regexPattern="[1-9]{1}[0-9]{5}"
+	if [[ $pin =~ $regexPattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+
+function enterPinCode() {
+	read -p "Enter PIN Code: " pinCode
+	validatePinCode $pinCode
+}
+
+enterPinCode
+
+sleep 1
+
+
